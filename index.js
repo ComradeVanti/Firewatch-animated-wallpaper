c421fb4d6c8dbd3c8bf01924beb070d059c75005
KingVanti <kingvanti650@gmail.com>
2021-07-02 20:44:11 +0200

    Add layer 9

--- index.js
+++ index.js
@@ -11,7 +11,8 @@
         {element: document.getElementById("layer5"), speed: 0.2},
         {element: document.getElementById("layer6"), speed: 0.1},
         {element: document.getElementById("layer7"), speed: 0.05},
-        {element: document.getElementById("layer8"), speed: 0.025}
+        {element: document.getElementById("layer8"), speed: 0.025},
+        {element: document.getElementById("layer9"), speed: 0.0125},
     ]
 
 
