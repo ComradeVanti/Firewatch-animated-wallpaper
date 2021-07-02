2ded4ba05518798971b1fd264ceb107412d717cf
KingVanti <kingvanti650@gmail.com>
2021-07-02 18:08:34 +0200

    Add movement logic for layers

---
+++ js/index.js
@@ -0,0 +1,37 @@
+﻿window.onload = function () {
+
+    const maxMovePercent = 10
+    const baseOffset = 10
+
+    const layers = [
+        {element: document.getElementById("layer1"), speed: 1}
+    ]
+
+
+    function interpolate(a, b, t) {
+        return a + (b - a) * t
+    }
+
+    function translateLayer(layer, offset) {
+        layer.element.style.transform = `translate(${offset}vw)`
+    }
+
+    function moveLayer(layer, xPercent) {
+        let t = xPercent * layer.speed
+        let offset = interpolate(maxMovePercent, -maxMovePercent, t) - baseOffset
+        translateLayer(layer, offset)
+    }
+
+    function moveLayers(xPercent) {
+        layers.forEach(layer => moveLayer(layer, xPercent))
+    }
+
+    function onMouseMoved(event) {
+        let x = event.clientX
+        let xPercent = x / window.innerWidth
+        moveLayers(xPercent)
+    }
+
+    document.onmousemove = onMouseMoved
+}
+
